8f9a505c0e5d29f0d26ed5fee14144cbd6d42a41
minechoo <choominhae@gmail.com>
2022-04-03 14:03:04 +0900

    hash

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
